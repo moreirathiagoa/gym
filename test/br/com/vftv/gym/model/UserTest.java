e9ac0f1d31212fa2ca0d607e2c124c2849935ace
Fellipe Maia <fellipe.apm@gmail.com>
2020-02-09 00:12:11 -0300

    Update class UserTest - criate method relaesePaymentNotDayTest

--- test/br/com/vftv/gym/model/UserTest.java
+++ test/br/com/vftv/gym/model/UserTest.java
@@ -7,6 +7,7 @@ package br.com.vftv.gym.model;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -52,11 +53,27 @@ public class UserTest {
         user.setCPF("11843771766");
         user.setRegistrationDate(sdf.parse("20/12/2000"));
         user.setExamExperationDate(sdf.parse("20/12/2020"));
-        user.setPayDay(sdf.parse("01/02/2020"));
+        user.setPayDay(Calendar.getInstance().getTime());
         //action
         Boolean result = user.isPaymentOnDay();
         //validation
         assertTrue(result);
+    }
+    
+    @Test
+    public void UserPaymentNotOnDayTest() throws ParseException{
+        //cenario
+        User user = new User();
+        user.setName("Fulano da Silva");
+        user.setBirthDate(sdf.parse("20/12/1985"));
+        user.setCPF("11843771766");
+        user.setRegistrationDate(sdf.parse("20/12/2000"));
+        user.setExamExperationDate(sdf.parse("20/12/2020"));
+        user.setPayDay(sdf.parse("07/01/2020"));
+        //action
+        Boolean result = user.isPaymentOnDay();
+        //validation
+        assertFalse(result);
     }
 
 }
